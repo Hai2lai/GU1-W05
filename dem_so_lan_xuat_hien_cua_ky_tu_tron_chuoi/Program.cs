5931bdece49e6ec2795a616c9551325fbf1b4e26
Hai2lai <152401246+Hai2lai@users.noreply.github.com>
2024-01-03 20:19:35 +0700

    .

---
+++ dem_so_lan_xuat_hien_cua_ky_tu_tron_chuoi/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Text;
+
+internal class Program
+{
+public static int DemSoLanXuatHien(char kyTu, string chuoi)
+{
+    int dem = 0;
+    foreach (char lol in chuoi)
+    {
+        if (lol == kyTu)
+        {
+            dem++;
+        }
+    }
+    return dem;
+}
+
+public static void Main()
+{   
+    Console.OutputEncoding=Encoding.UTF8;
+    Console.WriteLine("Nhập chuỗi:");
+    string chuoi = Console.ReadLine();
+
+    Console.WriteLine("Nhập ký tự:");
+    char kyTu = Console.ReadKey().KeyChar;
+
+    int soLanXuatHien = DemSoLanXuatHien(kyTu, chuoi);
+    Console.WriteLine("\nSố lần xuất hiện của ký tự '{0}' trong chuỗi là: {1}", kyTu, soLanXuatHien);
+
+    Console.ReadLine();
+}
+}
---
+++ su_khac_nhau_cua_tham_tri_va_tham_chieu/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static int Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        int value = 5;
+        PassByValue(value);
+        // Giá trị của "value" vẫn là 5
+        Console.WriteLine("Tham trị : "+value); 
+
+        PassByReference(ref value);
+        // Giá trị của "value" thay đổi thành 10
+        Console.WriteLine("Tham chiếu : "+value);
+
+        // cách không cần dấu cộng
+        int a=3;
+        Console.WriteLine("a = {0}",a);
+
+        return 0;
+    }
+    static int PassByValue(int number)
+    {
+        number = 10; // Không ảnh hưởng đến giá trị ban đầu của biến bên ngoài phương thức(tham trị)
+        return(number);
+    }
+
+    static int PassByReference(ref int number)
+    {
+        number = 10; // Thay đổi giá trị của biến ban đầu(tham chiếu)
+        return(number);
+    }
+}
